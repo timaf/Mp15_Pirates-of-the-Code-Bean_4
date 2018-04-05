37f015ec94b0d72be37af789fb96932ca96490f1
timaf <lo_sa_na@yahoo.com>
2018-04-05 20:25:07 +0200

    start

---
+++ src/main/java/at/refugeescode/Mp15_Pirates/of/the/CodeBean_4/Mp15PiratesOfTheCodeBean4Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp15_Pirates.of.the.CodeBean_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp15PiratesOfTheCodeBean4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp15PiratesOfTheCodeBean4Application.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/Mp15_Pirates/of/the/CodeBean_4/Mp15PiratesOfTheCodeBean4ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.Mp15_Pirates.of.the.CodeBean_4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp15PiratesOfTheCodeBean4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
